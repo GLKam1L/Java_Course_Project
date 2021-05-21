85525918b765c1454597875ac7b9fa066145dad5
GLKam1L <glkamil.17@gmail.com>
2021-05-21 04:13:14 +0300

    Добавлены контроллеры и роли

---
+++ src/main/java/ru/mirea/tourismapp/controller/HomeController.java
@@ -0,0 +1,40 @@
+package ru.mirea.tourismapp.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.mirea.tourismapp.domain.User;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String index(@AuthenticationPrincipal User user, Model model){
+        if (user!= null){
+            model.addAttribute("user",user.getUsername());
+            return "index";
+        }
+
+        model.addAttribute("user","anonymous");
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("/foruser")
+    public String forUser() {
+        return "foruser";
+    }
+
+    @GetMapping("/foradmin")
+    public String forAdmin() {
+
+        return "foradmin";
+    }
+}
---
+++ src/main/java/ru/mirea/tourismapp/repo/UserRepo.java
@@ -0,0 +1,8 @@
+package ru.mirea.tourismapp.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import ru.mirea.tourismapp.domain.User;
+
+public interface UserRepo extends CrudRepository<User,Long> {
+    User findByUsername(String name);
+}
